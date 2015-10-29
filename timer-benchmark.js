79579fd4e5e5a47faff3ea39228a62d3f3393851
nmrony <pro.nmrony@gmail.com>
2015-10-29 16:26:54 +0600

    added timer benchmark file

---
+++ timer-benchmark.js
@@ -0,0 +1,31 @@
+
+var fs = require('fs');
+var EventEmitter = require('events').EventEmitter;
+var pos = 0;
+var messenger = new EventEmitter();
+
+messenger.on('message', function(msg) { console.log(++pos + " message:" + msg);
+});
+
+console.log(++pos + " first");
+
+process.nextTick(function(){
+    console.log(++pos + " nextTick");
+});
+
+messenger.emit('message', "hello!");
+fs.stat(__filename, function(){
+    console.log(++pos + " stat");
+});
+
+setTimeout(function(){
+    console.log(++pos + " quick timer");
+}, 0);
+setTimeout(function(){
+    console.log(++pos + " long timer");
+}, 30);
+setImmediate(function(){
+    console.log(++pos + " immediate");
+});
+
+console.log(++pos + " last");
